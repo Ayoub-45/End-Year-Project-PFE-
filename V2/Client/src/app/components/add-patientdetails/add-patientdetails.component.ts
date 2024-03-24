d806f42d7eb166434ebf109cf24d4e947ca81a9c
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-24 23:45:45 +0100

    customize add patient details component

--- V2/Client/src/app/components/add-patientdetails/add-patientdetails.component.ts
+++ V2/Client/src/app/components/add-patientdetails/add-patientdetails.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { Component, input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { PatientService } from '../../services/patient.service';
 
 @Component({
   selector: 'app-add-patientdetails',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-patientdetails.component.html',
   styleUrl: './add-patientdetails.component.css',
 })
-export class AddPatientdetailsComponent {}
+export class AddPatientdetailsComponent {
+  constructor(public service: PatientService) {}
+}
