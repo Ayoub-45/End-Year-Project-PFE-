953912e0fc8f7010c1943ab75d5ad113b43b17da
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-03 07:50:06 +0100

    generate report list component

---
+++ V2/Client2/frontend/src/app/components/report-list/report-list/report-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-list.component.html',
+  styleUrl: './report-list.component.css'
+})
+export class ReportListComponent {
+
+}
