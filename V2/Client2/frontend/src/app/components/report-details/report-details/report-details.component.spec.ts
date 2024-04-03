ee553dc8e93ee952c09172f9c47a83f68fa5f3f8
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-03 07:51:29 +0100

    generate report details

---
+++ V2/Client2/frontend/src/app/components/report-details/report-details/report-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReportDetailsComponent } from './report-details.component';
+
+describe('ReportDetailsComponent', () => {
+  let component: ReportDetailsComponent;
+  let fixture: ComponentFixture<ReportDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReportDetailsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ReportDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ V2/Client2/frontend/src/app/components/report-details/report-details/report-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-details.component.html',
+  styleUrl: './report-details.component.css'
+})
+export class ReportDetailsComponent {
+
+}
