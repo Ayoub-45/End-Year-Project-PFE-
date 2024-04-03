3adb6612123a7e6c67dcefb146e6017f7aee3fc1
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-03 08:13:56 +0100

    Create the patient service and update the environment

---
+++ V2/Client2/frontend/src/app/interfaces/patient.ts
@@ -0,0 +1,16 @@
+export interface Patient {
+  id: number;
+  code: string;
+  nom: string;
+  prenom: string;
+  sexe: string;
+  adresse: string;
+  profession: string;
+  gs: string;
+  rh: number;
+  race: string;
+  poids: number;
+  taille: number;
+  statutMatrimonial: string;
+  date_Naissance: Date;
+}
