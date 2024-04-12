8eb417f03ec63e9f3b8fc47408aabcf883c2c0f9
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-12 10:39:14 +0100

    Create Home page

--- V2/medical-management/src/app/app.routes.ts
+++ V2/medical-management/src/app/app.routes.ts
@@ -5,18 +5,11 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 
 export const routes: Routes = [
   {
-    path: ``,
-    component: AppComponent,
-    title: 'Home',
-  },
-  {
     path: `signin`,
     component: SignInComponent,
-    title: 'sign-in',
   },
   {
     path: `patientmanagement`,
     component: PatientMangementComponent,
-    title: 'sign-in',
   },
 ];
