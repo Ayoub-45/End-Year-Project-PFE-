f81c7dd958915df8184cbd6331a5f516a5114ba2
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-12 10:54:07 +0100

    Home page

--- V2/medical-management/src/app/app.component.ts
+++ V2/medical-management/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- V2/medical-management/src/app/app.routes.ts
+++ V2/medical-management/src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { PatientMangementComponent } from './components/patient-mangement/patient-mangement.component';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
   {
     path: `signin`,
     component: SignInComponent,
