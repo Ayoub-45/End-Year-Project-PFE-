8ad6f1a8c2e9974716b7e26d5349c27b8b9fb52a
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-27 22:37:57 +0100

    Add a reusible component

--- V2/client2/src/app/app.component.ts
+++ V2/client2/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
-import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
+import { LoadingScreenComponent } from './reusable-components/loading-screen/loading-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SignInComponent,LoadingScreenComponent],
+  imports: [RouterOutlet, SignInComponent, LoadingScreenComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client2';
