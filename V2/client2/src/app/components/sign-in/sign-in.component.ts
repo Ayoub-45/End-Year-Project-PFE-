52ddfa09debdc03bdac7f3198b95e2982f6a8467
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-27 22:40:46 +0100

    Add logo as reusible component

--- V2/client2/src/app/components/sign-in/sign-in.component.ts
+++ V2/client2/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from '../logo/logo.component';
+import { LogoComponent } from '../../reusable-components/logo/logo.component';
 
 @Component({
   selector: 'app-sign-in',
