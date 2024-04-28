da4f12a4cfa3c8121246f071363993a867140509
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-28 02:40:17 +0100

    Create a service

--- V2/client2/src/app/components/about/about.component.ts
+++ V2/client2/src/app/components/about/about.component.ts
@@ -8,4 +8,11 @@ import { LogoComponent } from '../../reusable-components/logo/logo.component';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  content = `Medical Clinics is your dedicated ally in the journey through thyroid
+  conditions. Our platform provides comprehensive support and resources
+  tailored specifically for individuals facing medical challenges related to
+  thyroid health. From expert insights to community-driven assistance, we're
+  committed to empowering you with the knowledge and tools needed to thrive
+  despite your condition. towards optimal health and vitality.`;
+}
