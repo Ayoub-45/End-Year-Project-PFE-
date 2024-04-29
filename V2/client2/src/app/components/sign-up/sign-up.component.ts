c9fb03d3de970777cf74ec9a81e0958684fd2284
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-29 20:42:23 +0100

    add form

--- V2/client2/src/app/components/sign-up/sign-up.component.ts
+++ V2/client2/src/app/components/sign-up/sign-up.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {
-
+export class SignUpComponent implements OnInit {
+  registrationForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required, Validators.minLength(6)],
+      confirmPassword: [
+        '',
+        [Validators.required, this.matchPasswordValidator.bind(this)],
+      ],
+    });
+  }
+  matchPasswordValidator(control: FormGroup): { [s: string]: boolean } | null {
+    if (control.value !== this.registrationForm.get('password')?.value) {
+      return { passwordDontMatch: true };
+    }
+    return null;
+  }
 }
